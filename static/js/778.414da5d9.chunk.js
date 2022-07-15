aa7621cc6ddb12ea11ec100f7d4ea72f25d4f0a4
OlegSubotin <subotinoleg0@gmail.com>
2022-07-15 15:29:33 +0000

    Deploying to gh-pages from @ OlegSubotin/goit-react-hw-08-phonebook@e7f6221d5f0aaa521c069a94c7184fb7c81c6768 🚀

---
+++ static/js/778.414da5d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[778],{7778:function(e,o,t){t.r(o),t.d(o,{default:function(){return r}});t(2791);var n="HomeView_wrapper__stmUG",_="HomeView_title__GXw8J",c=t(184),r=function(){return(0,c.jsx)("div",{className:n,children:(0,c.jsx)("h1",{className:_,children:"Welcome to Phonebook App"})})}}}]);
+//# sourceMappingURL=778.414da5d9.chunk.js.map
