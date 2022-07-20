0f2b2856ae2378fd197eb0dd5458bc268d02fe3f
OlegSubotin <subotinoleg0@gmail.com>
2022-07-20 13:53:39 +0300

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,23 +1,3 @@
-// const getIsLoggedIn = state => state.auth.isLoggedIn;
-
-// const getUserName = state => state.auth.user.name;
-
-// const getUserEmail = state => state.auth.user.email;
-
-// // const getToken = state => state?.auth.token;
-
-// const getIsFetchingCurrentUser = state => state.auth.isFetchingCurrentUser;
-
-// const authSelectors = {
-//     getIsLoggedIn,
-//     getUserName,
-//     getUserEmail,
-//     // getToken,
-//     getIsFetchingCurrentUser,
-// };
-
-// export default authSelectors;
-
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUserName = state => state.auth.user.name;
